67efdc5948a08a95430cb248716b98f280b68a34
hgoh2 <henry.goh@macquarie.com>
2015-11-17 21:17:44 +1100

    FRAME1 - Added dynamic service loading

--- src/view/res/js/_parent.js
+++ src/view/res/js/_parent.js
@@ -1,8 +1,9 @@
 // Declare the app module
 var miaApp = angular.module('mia', []);
 
-miaApp.config(['$controllerProvider', function($controllerProvider) { 
+miaApp.config(['$controllerProvider', '$provide', function($controllerProvider, $provide) { 
     miaApp.registerCtrl = $controllerProvider.register;
+    miaApp.registerFactory = $provide.factory;
 }]);
 
 miaApp.controller('parentController', 
