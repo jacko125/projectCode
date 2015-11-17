c710a5ab5d95356f720dbed4415b4d59b0b5d383
hgoh2 <henry.goh@macquarie.com>
2015-11-18 10:20:58 +1100

    CONN1 - Initial commit for websocket connections

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var nunjucks = require('nunjucks');
+var util = require('util');
 
 // Instantiate the app
 var app = express();
@@ -21,4 +22,29 @@ var server = app.listen(3000, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+}); 
+
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ port: 3001 });
+
+wss.on('connection', function connection(ws) {
+      
+  var currentMsg = 'start';
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);    
+    
+    if (message == 'close') {
+        ws.close();
+    }
+    currentMsg = message;
+  });
+  
+  ws.on('close', function close() {
+    console.log('disconnected');
+  });
+
+    ws.send('Returning: ' + currentMsg);  
 });
+
+
+
