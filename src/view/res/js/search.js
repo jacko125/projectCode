c9d127fb9e16712c7c585da3801e0d459193ac00
Jackson Chan <jackson.chan@macquarie.com>
2015-11-17 14:53:15 +1100

    Attempt on retrieving via $http get to the searchStaff API. Unable to inject dependencies into searchController

--- src/view/res/js/search.js
+++ src/view/res/js/search.js
@@ -1,7 +1,16 @@
-miaApp.registerCtrl('searchController', [function() { 
+var miaApp = angular.module('mia');
+
+miaApp.registerCtrl('searchController', [function($scope, staffSearchService) {
         var self = this;        
         
         self.title = 'Staff Search'
         self.message = "This page allows you to search for other Macquarie employees";
-                
+
+
+        self.loadStaffList = function(name) {
+            staffSearchService.getStaffList(name).then(function(results)
+            {
+//                $scope.staffList = results;
+            });
+        };
 }]);
---
+++ src/view/res/js/staffSearchService.js
@@ -0,0 +1,27 @@
+// Declare the app module
+var miaApp = angular.module('mia');
+
+app.factory('staffSearchService', ['$http', function($http) {
+
+    var defaultStaffProfileData = {
+        'firstName': = ''
+        'lastName': = ''
+    };
+
+    var getStaffList = function(name) {
+
+        var params = {
+            'name': name
+        };
+
+        $http.get('http://staffdirectory.pc.internal.macquarie.com/WebServices/SearchService.svc/SearchStaff', {
+            params: params
+        }).success(function (result) {
+            console.log(result);
+        })
+    };
+
+    return {
+    getStaffList: getStaffList
+    };
+}])
