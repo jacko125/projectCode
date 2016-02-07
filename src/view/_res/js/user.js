226fb47c9c1556575a4c1442243baae5e367ec77
hgoh2 <henry.goh@macquarie.com>
2016-02-08 10:16:58 +1100

    Implement basic "User profile" page

---
+++ src/view/_res/js/user.js
@@ -0,0 +1,8 @@
+miaApp.controller('userController', [
+    '$http', '$window', '$scope','$rootScope', '$state', '$stateParams',   
+    function($http, $window, $scope, $rootScope, $state, $stateParams) {   
+        var self = this;        
+        
+        
+    }
+]);
